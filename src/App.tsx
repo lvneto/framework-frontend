7f09720e326f53d36ffafe643926d4cdd02d4fb7
lvneto <lauro.neto@irriga.net>
2022-05-10 00:20:20 -0300

    fix prime dividers render

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { FormEvent, useState } from 'react';
 import { Loading } from './component/Loading';
 import { api } from './lib/api';
 import * as React from 'react';
-import { JsonView, darkStyles, defaultStyles } from 'react-json-view-lite';
+import { JsonView, darkStyles } from 'react-json-view-lite';
 import 'react-json-view-lite/dist/index.css';
+import { PaginatedList } from 'react-paginated-list';
 
 interface DataDividers{ 
   data: {
@@ -14,9 +15,10 @@ interface DataDividers{
 } 
 
 export function App() {
+
   const [number, setNumber] = useState('')
   const [isSendingNumber, setIsSendingNumber] = useState(false)
-  const [dividers, setDividers] = useState<DataDividers>() || undefined; 
+  const [dividers, setDividers] = useState<DataDividers>(); 
 
   async function handleSubmitNumber(event: FormEvent) {
     event.preventDefault();
@@ -35,32 +37,57 @@ export function App() {
     });
 
     setIsSendingNumber(false);
-  }
-
-  function handleWithCleanDividers () {
-    const clean = [] as any
-
-    setDividers(clean)
-  }
+  } 
   
   return (
     <>
       <div className="grid place-items-center h-screen">
         <form onSubmit={handleSubmitNumber}>
           <div className="flex items-center border-b border-teal-500 py-2">
-            <input onChange={event => setNumber(event.target.value)} className="appearance-none border-4 text-gray-600 py-1 px-2 rounded  bg-white border-none w-full text-white-500 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Coloque aqui seu número (até 5 números)" aria-label="Full name"/>
-            <button disabled={isSendingNumber} className="flex-shrink-0 mr-3 bg-white hover:bg-gray-300 border-white hover:border-gray-600 text-sm border-4 text-gray-600 py-1 px-2 rounded" type="submit">
+            <input onChange={event => setNumber(event.target.value)}
+             className="appearance-none border-4 text-gray-600 py-1 px-2 rounded
+               bg-white border-none w-full text-white-500 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                placeholder="Coloque aqui seu número (até 5 números)" aria-label="Full name"/>
+            <button disabled={isSendingNumber} className="flex-shrink-0 mr-3 bg-white hover:bg-gray-300
+             border-white hover:border-gray-600 text-sm border-4 text-gray-600 py-1 px-2 rounded" type="submit">
             { isSendingNumber ? <Loading/> : 'Enviar Número' }
             </button>  
-            <button onClick={handleWithCleanDividers} className="flex-shrink-0 bg-white hover:bg-gray-300 border-white hover:border-gray-600 text-sm border-4 text-gray-600 py-1 px-2 rounded" type="reset">Limpar</button>         
-          </div>
+            <button className="flex-shrink-0 bg-white hover:bg-gray-300
+             border-white hover:border-gray-600 text-sm border-4 text-gray-600 py-1 px-2 rounded" type="reset">Limpar</button>         
+          </div>    
           <div>
             <React.Fragment>
-              <JsonView data={dividers ? dividers : 'Nenhum número'} shouldInitiallyExpand={(level) => true} style={darkStyles} />
-            </React.Fragment>
+              <span>
+                Número:<JsonView data={dividers ?  dividers.data.number : 'Nenhum número inserido'} shouldInitiallyExpand={() => true} style={darkStyles} /> 
+              </span>
+              <span>
+                Divisores:
+                 <JsonView data={dividers ? dividers.data.dividers : 'Nenhum número'} shouldInitiallyExpand={() => false} style={darkStyles} />
+              </span>               
+            </React.Fragment>  
           </div>  
+          <span>Divisores primos</span> 
+
+          {dividers ? <PaginatedList
+            list={dividers ? dividers.data.primeDividers : []}
+            itemsPerPage={10}
+            renderList={(list) => (
+              <>
+                {list.map((item, id) => {
+                  return (
+                    <div key={id}>
+                      {item}
+                    </div>
+                  );
+                })}
+              </>
+            )}
+          />  : null }               
+           
         </form>
-      </div>             
+      </div>              
     </>
   )
 }
+
+
